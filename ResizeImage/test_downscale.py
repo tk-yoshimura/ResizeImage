b7f2512d316793c966417b7045f29e787d6791a4
tk-yoshimura <yoshimura8192@gmail.com>
2025-04-12 02:22:59 +0900

    fix: test param

--- ResizeImage/test_downscale.py
+++ ResizeImage/test_downscale.py
@@ -7,8 +7,8 @@ import unittest
 class Test_downscale(unittest.TestCase):
     def test_resize_x0p75_withchannel_float32(self):
         img = np.zeros((64, 48, 3), dtype=np.uint8)
-        img = cv2.circle(img, (24, 16), 8, color=(255, 128, 64), thickness=-1)
-        img = cv2.circle(img, (24, 48), 12, color=(128, 64, 255), thickness=-1)
+        img = cv2.circle(img, (24, 16), 20, color=(255, 128, 64), thickness=-1)
+        img = cv2.circle(img, (24, 48), 16, color=(128, 64, 255), thickness=-1)
 
         img_resize = resize_x0p75(img.astype(np.float32))
         img_cv2_resize = cv2.resize(img.astype(np.float32), (36, 48), interpolation=cv2.INTER_AREA)
@@ -17,8 +17,8 @@ class Test_downscale(unittest.TestCase):
 
     def test_resize_x0p75_withchannel_float64(self):
         img = np.zeros((64, 48, 3), dtype=np.uint8)
-        img = cv2.circle(img, (24, 16), 8, color=(255, 128, 64), thickness=-1)
-        img = cv2.circle(img, (24, 48), 12, color=(128, 64, 255), thickness=-1)
+        img = cv2.circle(img, (24, 16), 20, color=(255, 128, 64), thickness=-1)
+        img = cv2.circle(img, (24, 48), 16, color=(128, 64, 255), thickness=-1)
 
         img_resize = resize_x0p75(img.astype(np.float64))
         img_cv2_resize = cv2.resize(img.astype(np.float64), (36, 48), interpolation=cv2.INTER_AREA)
@@ -27,8 +27,8 @@ class Test_downscale(unittest.TestCase):
 
     def test_resize_x0p75_withoutchannel_float32(self):
         img = np.zeros((64, 48), dtype=np.uint8)
-        img = cv2.circle(img, (24, 16), 8, color=255, thickness=-1)
-        img = cv2.circle(img, (24, 48), 12, color=64, thickness=-1)
+        img = cv2.circle(img, (24, 16), 20, color=255, thickness=-1)
+        img = cv2.circle(img, (24, 48), 16, color=64, thickness=-1)
 
         img_resize = resize_x0p75(img.astype(np.float32))
         img_cv2_resize = cv2.resize(img.astype(np.float32), (36, 48), interpolation=cv2.INTER_AREA)
@@ -37,8 +37,8 @@ class Test_downscale(unittest.TestCase):
 
     def test_resize_x0p75_withoutchannel_float64(self):
         img = np.zeros((64, 48), dtype=np.uint8)
-        img = cv2.circle(img, (24, 16), 8, color=255, thickness=-1)
-        img = cv2.circle(img, (24, 48), 12, color=64, thickness=-1)
+        img = cv2.circle(img, (24, 16), 20, color=255, thickness=-1)
+        img = cv2.circle(img, (24, 48), 16, color=64, thickness=-1)
 
         img_resize = resize_x0p75(img.astype(np.float64))
         img_cv2_resize = cv2.resize(img.astype(np.float64), (36, 48), interpolation=cv2.INTER_AREA)
